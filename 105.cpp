4dd81598816b8f371ad9cf5009c7f41a8b649f0d
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-05-04 11:25:59 +0400

    Create 105.cpp

---
+++ 105.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout << "          Seoul     10,312,545        +91,375" << std::endl;
+    std::cout << "          Pusan      3,567,910         +5,868" << std::endl;
+    std::cout << "        Incheon      2,758,296        +64,888" << std::endl;
+    std::cout << "          Daegu      2,511,676        +17,230" << std::endl;
+    std::cout << "        Gwangju      1,454,636        +29,774";
+    return 0;
+}
