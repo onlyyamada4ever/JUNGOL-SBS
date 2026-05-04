8848c4ddd625a3b6ebe0f8b41d5fd7c9383ee74b
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-05-04 11:18:57 +0400

    Add program to display subject scores

---
+++ 104.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "kor 90" << std::endl;
+    std::cout << "mat 80" << std::endl;
+    std::cout << "eng 100" << std::endl;
+    std::cout << "sum 270" << std::endl;
+    return 0;
+}
