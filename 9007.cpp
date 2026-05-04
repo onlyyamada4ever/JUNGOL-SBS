c4a7332faf8adc6270b06d0d73f98a0496df3ed0
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-05-04 10:59:16 +0400

    Create 9007.cpp

---
+++ 9007.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    std::cout << "수식을 출력하면 계산 결과가 출력됩니다. \n";
+    std::cout << "10 + 5 = " << 10 + 5 << std::endl;
+    std::cout << "내 생일은 " << 2005 << "년 " << 6 << "월 ";
+    std::cout << 17 << "일 입니다.";
+}
