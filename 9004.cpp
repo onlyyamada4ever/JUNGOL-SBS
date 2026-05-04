50b7eb3f06e34377be67fe4604ec72705aed0d26
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-05-04 10:26:16 +0400

    Create 9004.cpp

---
+++ 9004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    std::cout << "   @@ " << std::endl;
+    std::cout << "  @  @ " << std::endl;
+    std::cout << " @    @ " << std::endl;
+    std::cout << "@      @ " << std::endl;
+    std::cout << " @    @ " << std::endl;
+    std::cout << "  @  @ " << std::endl;
+    std::cout << "   @@";
+    // for문을 써서 할까 했는데 귀찮아서 그냥 일일히 쓰기로 햇다.
+    // 진정한 고수는 에겐스러운 for문따윈 쓰지 않아 엘삣삐.
+    return 0;
+}
