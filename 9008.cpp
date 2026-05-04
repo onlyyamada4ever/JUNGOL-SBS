3f7aa77573875354d96774516018778f6de06d5c
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-05-04 11:10:22 +0400

    Create 9008.cpp

---
+++ 9008.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout << " subject score " << std::endl;
+    std::cout << "==============" << std::endl;
+    std::cout << "  korean    90" << std::endl;
+    std::cout << " english   100" << std::endl;
+    std::cout << "computer    80";
+    return 0;
+}
