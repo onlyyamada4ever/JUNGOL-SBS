218d96b14e027906adc11c955fac6a3a811b437c
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-05-04 11:13:16 +0400

    Create 508.cpp

---
+++ 508.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "      item     count     price" << std::endl;
+    std::cout << "       pen        20       100" << std::endl;
+    std::cout << "      note         5        95" << std::endl;
+    std::cout << "    eraser       110        97";
+    return 0;
+}
